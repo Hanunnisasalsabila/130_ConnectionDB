67223d9e0121c46d2609fa2a2cd8f40a524f7cb9
Hanunnisasalsabila <hanunsalsabila83@gmail.com>
2025-10-15 12:29:22 +0700

    buat endpoint GET /biodata untuk mengambil semua data dari tabel biodata

--- index.js
+++ index.js
@@ -25,6 +25,17 @@ app.get('/', (req, res) => {
   res.send('Selamat datang di API Biodata!');
 });
 
+app.get('/biodata', (req, res) => {
+  const sql = "SELECT * FROM biodata";
+  db.query(sql, (err, results) => {
+    if (err) {
+      console.error('Error fetching data:', err);
+      return res.status(500).json({ message: 'Gagal mengambil data dari database' });
+    }
+    res.status(200).json(results);
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
